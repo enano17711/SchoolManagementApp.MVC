c72db7bdf07756a0577b664ae2a5b4c43ce1e732
Juan Pablo Rodriguez <enano17711@gmail.com>
2023-06-07 00:10:14 -0400

    feat: Classes scaffold

---
+++ SchoolManagementApp.MVC/Controllers/ClassesControles.cs
@@ -0,0 +1,203 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagementApp.MVC.Context;
+using SchoolManagementApp.MVC.Data;
+
+namespace SchoolManagementApp.MVC.Controllers
+{
+    public class ClassesControles : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public ClassesControles(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ClassesControles
+        public async Task<IActionResult> Index()
+        {
+            var myDbContext = _context.Classes.Include(c => c.Course)
+                .Include(c => c.Lecturer);
+            return View(await myDbContext.ToListAsync());
+        }
+
+        // GET: ClassesControles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Classes == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Classes
+                .Include(c => c.Course)
+                .Include(c => c.Lecturer)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            return View(@class);
+        }
+
+        // GET: ClassesControles/Create
+        public IActionResult Create()
+        {
+            ViewData["CourseId"] = new SelectList(_context.Courses,
+                "Id",
+                "Id");
+            ViewData["LecturerId"] = new SelectList(_context.Lectures,
+                "Id",
+                "Id");
+            return View();
+        }
+
+        // POST: ClassesControles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,LecturerId,CourseId,Time")] Class @class)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@class);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["CourseId"] = new SelectList(_context.Courses,
+                "Id",
+                "Id",
+                @class.CourseId);
+            ViewData["LecturerId"] = new SelectList(_context.Lectures,
+                "Id",
+                "Id",
+                @class.LecturerId);
+            return View(@class);
+        }
+
+        // GET: ClassesControles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Classes == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Classes.FindAsync(id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["CourseId"] = new SelectList(_context.Courses,
+                "Id",
+                "Id",
+                @class.CourseId);
+            ViewData["LecturerId"] = new SelectList(_context.Lectures,
+                "Id",
+                "Id",
+                @class.LecturerId);
+            return View(@class);
+        }
+
+        // POST: ClassesControles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,
+            [Bind("Id,LecturerId,CourseId,Time")] Class @class)
+        {
+            if (id != @class.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@class);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClassExists(@class.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["CourseId"] = new SelectList(_context.Courses,
+                "Id",
+                "Id",
+                @class.CourseId);
+            ViewData["LecturerId"] = new SelectList(_context.Lectures,
+                "Id",
+                "Id",
+                @class.LecturerId);
+            return View(@class);
+        }
+
+        // GET: ClassesControles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Classes == null)
+            {
+                return NotFound();
+            }
+
+            var @class = await _context.Classes
+                .Include(c => c.Course)
+                .Include(c => c.Lecturer)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (@class == null)
+            {
+                return NotFound();
+            }
+
+            return View(@class);
+        }
+
+        // POST: ClassesControles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Classes == null)
+            {
+                return Problem("Entity set 'MyDbContext.Classes'  is null.");
+            }
+
+            var @class = await _context.Classes.FindAsync(id);
+            if (@class != null)
+            {
+                _context.Classes.Remove(@class);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClassExists(int id)
+        {
+            return (_context.Classes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
