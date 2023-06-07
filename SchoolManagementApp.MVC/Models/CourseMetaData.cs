b27eef60a4c0e2b8f833fcabeb3621bfb6b5b5e2
Juan Pablo Rodriguez <enano17711@gmail.com>
2023-06-06 23:59:00 -0400

    feat:styled and building page from better look

---
+++ SchoolManagementApp.MVC/Models/CourseMetaData.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagementApp.MVC.Data;
+
+public class CourseMetaData
+{
+    [Display(Name = "Course")] public string Name { get; set; } = null!;
+
+    [Display(Name = "Code")] public string? Code { get; set; }
+
+    [Display(Name = "Points")] public int? Credits { get; set; }
+}
+
+[ModelMetadataType(typeof(CourseMetaData))]
+public partial class Course
+{
+}
