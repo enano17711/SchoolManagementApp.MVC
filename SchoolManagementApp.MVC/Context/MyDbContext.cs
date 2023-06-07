400656ae105f27884dc2ca26c9fa3071237df76a
Juan Pablo Rodriguez <enano17711@gmail.com>
2023-06-07 00:00:00 -0400

    feat: create tables lecture and class, also modify other models

--- SchoolManagementApp.MVC/Context/MyDbContext.cs
+++ SchoolManagementApp.MVC/Context/MyDbContext.cs
@@ -12,14 +12,31 @@ public partial class MyDbContext : DbContext
     {
     }
 
-    public virtual DbSet<Course>? Courses { get; set; }
+    public virtual DbSet<Class> Classes { get; set; }
 
-    public virtual DbSet<Lecture>? Lectures { get; set; }
+    public virtual DbSet<Course> Courses { get; set; }
 
-    public virtual DbSet<Student>? Students { get; set; }
+    public virtual DbSet<Enrollment> Enrollments { get; set; }
+
+    public virtual DbSet<Lecture> Lectures { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Class>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Classes__3214EC0795F0362A");
+
+            entity.HasOne(d => d.Course).WithMany(p => p.Classes)
+                .HasForeignKey(d => d.CourseId)
+                .HasConstraintName("FK__Classes__CourseI__5DCAEF64");
+
+            entity.HasOne(d => d.Lecturer).WithMany(p => p.Classes)
+                .HasForeignKey(d => d.LecturerId)
+                .HasConstraintName("FK__Classes__Lecture__5CD6CB2B");
+        });
+
         modelBuilder.Entity<Course>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PK__Courses__3214EC079FE2F7DA");
@@ -32,6 +49,21 @@ public partial class MyDbContext : DbContext
             entity.Property(e => e.Name)
                 .HasMaxLength(50)
                 .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Enrollment>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Enrollme__3214EC078FCCE552");
+
+            entity.Property(e => e.Grade).HasMaxLength(2);
+
+            entity.HasOne(d => d.Class).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.ClassId)
+                .HasConstraintName("FK__Enrollmen__Class__619B8048");
+
+            entity.HasOne(d => d.Student).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.StudentId)
+                .HasConstraintName("FK__Enrollmen__Stude__60A75C0F");
         });
 
         modelBuilder.Entity<Lecture>(entity =>
